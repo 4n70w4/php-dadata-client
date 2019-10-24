a8510fefbafa8bef4a8af05b7f58148fe321fa6d
Krot Eval <38257723+4n70w4@users.noreply.github.com>
2019-10-24 12:13:20 +0300

    init

---
+++ src/DaDataClientFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace DadataClient;
+
+
+use BaseClient\BaseClientFactory;
+use GuzzleHttp\RequestOptions;
+
+class DaDataClientFactory extends BaseClientFactory {
+
+
+
+    public function getTransport($token, $base_uri = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/') {
+        $headers = [
+            'Content-Type' => 'application/json',
+            'Authorization' => "Token {$token}",
+        ];
+
+        return $this->getClient($base_uri, false, [RequestOptions::HEADERS => $headers]);
+    }
+
+
+
+}
